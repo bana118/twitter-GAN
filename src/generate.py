b065e34064728078e84abadf5129c186ff51e60c
baba118 <tachibana.k.ae@m.titech.ac.jp>
2019-01-25 17:28:36 +0900

    コピペした

--- src/generate.py
+++ src/generate.py
@@ -5,13 +5,13 @@ from keras.layers.core import Activation, Dense, Dropout
 from keras.layers.cudnn_recurrent import CuDNNLSTM
 from keras.layers.recurrent import LSTM
 from keras.models import Sequential
-from keras.callbacks import ModelCheckpoint
+from keras.callbacks import ModelCheckpoint, LambdaCallback
 from keras import regularizers
 from keras.optimizers import RMSprop
 
 from janome.tokenizer import Tokenizer
 import collections
-
+import random, sys
 
 
 def CuDNNLSTMmodel():
@@ -75,7 +75,58 @@ def test():
     X = np.load("data/test_X.npy")
     y = np.load("data/test_y.npy")
     model = LSTMmodel(X.shape[1], X.shape[2])
-    model.fit(X, y, batch_size=128, epochs=1)
+    print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+    model.fit(X, y, batch_size=128, epochs=60)
+
+def on_epoch_end(epoch, logs):
+    # Function invoked at end of each epoch. Prints generated text.
+    print()
+    print('----- Generating text after Epoch: %d' % epoch)
+
+    # モデルは40文字の「文」からその次の「字」を予測するものであるため、
+    # その元となる40文字の「文」を入力テキストからランダムに選ぶ
+    start_index = random.randint(0, len(text) - maxlen - 1)
+
+    # diversityとは多様性を意味する言葉
+    # この値が低いとモデルの予測で出現率が高いとされた「字」がそのまま選ばれ、
+    # 高ければそうでない「字」が選ばれる確率が高まる
+    for diversity in [0.2, 0.5, 1.0, 1.2]:
+        print('----- diversity:', diversity)
+
+        generated = ''
+
+        # 元にする「文」を選択
+        sentence = text[start_index: start_index + maxlen]
+        generated += sentence
+        print('----- Generating with seed: "' + sentence + '"')
+        sys.stdout.write(generated)
+    for i in range(200):
+
+        # 現在の「文」の中のどの位置に何の「字」があるかのテーブルを
+        # フィッティング時に入力したxベクトルと同じフォーマットで生成
+        # 最初の次元は「文」のIDなので0固定
+        x_pred = np.zeros((1, maxlen, len(chars)))
+        for t, char in enumerate(sentence):
+            x_pred[0, t, char_indices[char]] = 1.
+
+        # 現在の「文」に続く「字」を予測する
+        preds = model.predict(x_pred, verbose=0)[0]
+        next_index = sample(preds, diversity)
+        next_char = indices_char[next_index]
+
+        # 予測して得られた「字」を生成し、「文」に追加
+        generated += next_char
+
+        # モデル入力する「文」から最初の文字を削り、予測結果の「字」を追加
+        # 例：sentence 「これはドイツ製」
+        #     next_char 「の」
+        #     ↓
+        #     sentence 「れはドイツ製の」
+        sentence = sentence[1:] + next_char
+
+        sys.stdout.write(next_char)
+        sys.stdout.flush()
+    print()
 
 if __name__ == "__main__":
     #f = open("data/nisizaki.txt", "r", encoding="UTF8")
