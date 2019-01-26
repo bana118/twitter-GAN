cc468c5689a3e6c6a183e321c9b0aa8a71047dc9
bana118 <[tachibana.k.ae@m.titech.ac.jp]>
2019-01-26 18:14:10 +0900

    一応動作するようになった。以下次にやること 1. モデルを改善する 2. コピペだらけのコードをきれいにする 3. エポックごとの文字予測をランダムな単語列からではなく入力された単語列から予測させるようにする

--- src/generate.py
+++ src/generate.py
@@ -13,18 +13,19 @@ from janome.tokenizer import Tokenizer
 import collections
 import random, sys
 
+#苦し紛れ
+char_indices = {}
+indices_char = {}
+text = ""
+chars = ""
+pre_model = Sequential()
 
-def CuDNNLSTMmodel():
+def CuDNNLSTMmodel(maxlen, nb):
     model = Sequential()
-    model.add(CuDNNLSTM(512, return_sequences=True, input_shape=(2,2)))
-    model.add(Dropout(0.2))
-    model.add(CuDNNLSTM(128, return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(CuDNNLSTM(64, return_sequences=True))
-    model.add(Dropout(0.2))
-    model.add(CuDNNLSTM(32))
-    model.add(Dropout(0.2))
-    model.add(Dense(2, activation='softmax'))
+    model.add(CuDNNLSTM(128, input_shape=(maxlen, nb)))
+    model.add(Dense(nb))
+    model.add(Activation("softmax"))
+    model.compile(loss='categorical_crossentropy', optimizer=RMSprop(lr=0.01))
     return model
 
 def LSTMmodel(maxlen, nb):
@@ -68,17 +69,31 @@ def generateToken(tweetlist):
         for j, tk in enumerate(input_token):
             X[i, j, tokens_to_num[tk]] = 1
         y[i, tokens_to_num[label_tokens[i]]] = 1
-    np.save("test_X.npy", X)
-    np.save("test_y.npy", y)
+    #np.save("data/test_X.npy", X)
+    #np.save("data/test_y.npy", y)
+    global char_indices, indices_char, text, chars, pre_model
+    char_indices = tokens_to_num
+    indices_char = num_to_tokens
+    text = list(tokens_to_num.keys())
+    chars = text
+    pre_model = CuDNNLSTMmodel(X.shape[1], X.shape[2])
+    print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
+    pre_model.fit(X, y, batch_size=128, epochs=60, callbacks=[print_callback])
 
 def test():
     X = np.load("data/test_X.npy")
     y = np.load("data/test_y.npy")
     model = LSTMmodel(X.shape[1], X.shape[2])
     print_callback = LambdaCallback(on_epoch_end=on_epoch_end)
-    model.fit(X, y, batch_size=128, epochs=60)
+    model.fit(X, y, batch_size=128, epochs=60, callbacks=[print_callback])
+
 
 def on_epoch_end(epoch, logs):
+    maxlen = 5
+    #f = open("data/nisizaki.txt", "r", encoding="UTF8")
+    #tweet_list = f.read().split("&split")
+    #text = Tokenizer().tokenize(" ".join(tweet_list), wakati=True)
+    #chars = text
     # Function invoked at end of each epoch. Prints generated text.
     print()
     print('----- Generating text after Epoch: %d' % epoch)
@@ -86,6 +101,7 @@ def on_epoch_end(epoch, logs):
     # モデルは40文字の「文」からその次の「字」を予測するものであるため、
     # その元となる40文字の「文」を入力テキストからランダムに選ぶ
     start_index = random.randint(0, len(text) - maxlen - 1)
+    #start_index = 0
 
     # diversityとは多様性を意味する言葉
     # この値が低いとモデルの予測で出現率が高いとされた「字」がそのまま選ばれ、
@@ -96,21 +112,21 @@ def on_epoch_end(epoch, logs):
         generated = ''
 
         # 元にする「文」を選択
-        sentence = text[start_index: start_index + maxlen]
+        sentence = "".join(text[start_index: start_index + maxlen])
         generated += sentence
         print('----- Generating with seed: "' + sentence + '"')
         sys.stdout.write(generated)
-    for i in range(200):
+    for i in range(50):
 
         # 現在の「文」の中のどの位置に何の「字」があるかのテーブルを
         # フィッティング時に入力したxベクトルと同じフォーマットで生成
         # 最初の次元は「文」のIDなので0固定
         x_pred = np.zeros((1, maxlen, len(chars)))
-        for t, char in enumerate(sentence):
+        for t, char in enumerate(text[start_index: start_index + maxlen]):
             x_pred[0, t, char_indices[char]] = 1.
 
         # 現在の「文」に続く「字」を予測する
-        preds = model.predict(x_pred, verbose=0)[0]
+        preds = pre_model.predict(x_pred, verbose=0)[0]
         next_index = sample(preds, diversity)
         next_char = indices_char[next_index]
 
@@ -128,8 +144,16 @@ def on_epoch_end(epoch, logs):
         sys.stdout.flush()
     print()
 
+def sample(preds, temperature=1.0):
+    # helper function to sample an index from a probability array
+    preds = np.asarray(preds).astype('float64')
+    preds = np.log(preds) / temperature
+    exp_preds = np.exp(preds)
+    preds = exp_preds / np.sum(exp_preds)
+    probas = np.random.multinomial(1, preds, 1)
+    return np.argmax(probas)
+
 if __name__ == "__main__":
-    #f = open("data/nisizaki.txt", "r", encoding="UTF8")
-    #tweet_list = f.read().split("&split")
-    #generateToken(tweet_list)
-    test()
+    f = open("data/nisizaki.txt", "r", encoding="UTF8")
+    tweet_list = f.read().split("&split")
+    generateToken(tweet_list)
